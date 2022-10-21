6aaf5bdb0711918b5231e713af60e61d403a2018
Nabil Ahmed <nabilahmed98@hotmail.com>
2022-10-21 00:11:07 +0000

    authentication of users name, email, password

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,16 @@ Rails.application.routes.draw do
     resources :categories
   end
 
+  # ROUTES -> login form, logging in, and logging out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # Signup routes 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
